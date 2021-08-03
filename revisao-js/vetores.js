7e162879144166734401bae4d1005c2691cbe028
Alexandre-Vilefort <alexandre.vilefot@gmail.com>
2021-08-03 15:14:20 +0000

    init

--- revisao-js/vetores.js
+++ revisao-js/vetores.js
@@ -4,3 +4,10 @@ frutas.push('morango');
 console.log(frutas);
 frutas.unshift('maracujá');
 console.log(frutas);
+frutas.splice(3,0,'mamão');
+console.log(frutas);
+
+// pop(): Remove an item from the end of an array.
+// push(): Add items to the end of an array.
+// shift(): Remove an item from the beginning of an array.
+// unshift(): Add items to the beginning of an array.
